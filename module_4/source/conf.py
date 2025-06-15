b9a542460f1efb7a4ff0701fc29b4786458dc3a4
Sara Nasab <sara.nasab@jhuapl.edu>
2025-06-15 11:11:38 -0400

    adding conf.py

--- module_4/source/conf.py
+++ module_4/source/conf.py
@@ -8,8 +8,10 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src'))
-sys.path.insert(0, os.path.abspath('..'))
+# Add the project root and src directory to the Python path
+# This allows Sphinx to find your Python modules for autodoc
+sys.path.insert(0, os.path.abspath('../src')) # Path to your src folder
+sys.path.insert(0, os.path.abspath('..'))    # Path to the module_4 folder (parent of source)
 
 project = 'Module 4: Pizza Ordering System'
 copyright = '2025, Sara Nasab'
@@ -18,11 +20,21 @@ author = 'Sara Nasab'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc']
+extensions = [
+    'sphinx.ext.autodoc', # Enables automatic documentation from docstrings
+]
+
+# Configure autodoc to include members, undoc-members, show-inheritance,
+# and crucially, var-members (for module-level variables like your price dictionaries)
+autodoc_default_options = {
+    'members': True,
+    'undoc-members': True,
+    'show-inheritance': True,
+    'var-members': True, # This tells autodoc to include module-level variables
+}
 
 templates_path = ['_templates']
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
